0ce51b41834587fa3607d337e2c68cff8c27179b
Nyumat <46255836+Nyumat@users.noreply.github.com>
2020-05-24 03:48:27 -0400

    Update checkIfOddOrEven.swift

--- checkIfOddOrEven.swift
+++ checkIfOddOrEven.swift
@@ -3,10 +3,10 @@ var randomNum = Int.random(in: 0.5000)
 
 switch randomNum { 
   case let x where x % 2 == 0:
-    print("\(randomNum) is even."
+    print("\(randomNum) is even.")
   case let x where x % 2 == 1:
-    print("\(randomNum) is odd."
+    print("\(randomNum) is odd.")
   default: 
-    print("Invalid Number."
+    print("Invalid Number.")
 }
 print(randomNum)
